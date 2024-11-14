a882a003114a6e905b0704890fdfb1afcde8416c
come_ill_foo <rayveleaveme34@inbox.ru>
2024-11-14 22:07:38 +0000

    feat(mergers): parallelize merge_matrices with OpenMP

--- src/mergers.c
+++ src/mergers.c
@@ -8,6 +8,45 @@ double merge_by_pow(double a, double b)
 }
 
 #define MIN(a, b) (((a) < (b))? (a) : (b))
+#ifdef _OPENMP
+int merge_matrices(struct matrix* restrict srcp, struct matrix* restrict dstp,
+                   merger fn)
+{
+    int ret = 0;
+    if (!srcp || !dstp) return -1;
+    const size_t rows = MIN(srcp->rows, dstp->rows);
+    const size_t cols = MIN(srcp->cols, dstp->cols);
+    #pragma omp parallel for collapse(2) default(none) shared(srcp, dstp, ret, fn, rows, cols)
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j) {
+            double a = 0.0;
+            double b = 0.0;
+            if (ret) continue;
+            if (double_matrix_get(srcp, i, j, &a)) {
+                #pragma omp critical
+                {
+                    ret = -1;
+                }
+                continue;
+            }
+            if (double_matrix_get(dstp, i, j, &b)) {
+                #pragma omp critical
+                {
+                    ret = -1;
+                }
+                continue;
+            }
+            if (double_matrix_set(dstp, i, j, fn(a, b))) {
+                #pragma omp critical
+                {
+                    ret = -1;
+                }
+                continue;
+            }
+        }
+    return ret;
+}
+#else
 int merge_matrices(struct matrix* restrict srcp, struct matrix* restrict dstp,
                    merger fn)
 {
@@ -26,4 +65,5 @@ int merge_matrices(struct matrix* restrict srcp, struct matrix* restrict dstp,
         }
     return ret;
 }
+#endif
 #undef MIN
