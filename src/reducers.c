c3a41a8bf0fae0a39b372b422a921289244927b8
come_ill_foo <rayveleaveme34@inbox.ru>
2024-11-28 23:47:53 +0000

    feat(src): parallelize reduction with pthreads

--- src/reducers.c
+++ src/reducers.c
@@ -10,8 +10,60 @@ static bool is_even_as_integer(double number)
     return !(((uint_least64_t) number) % 2);
 }
 
+#ifdef USE_PTHREAD
+#include "ptpool.h"
+extern struct ptpool* pool;
 
-#ifdef _OPENMP
+struct _reduce_vec_args
+{
+    struct matrix* matp;
+    size_t row;
+    double X;
+};
+
+static void* _reduce_vec_routine(void* args)
+{
+    struct _reduce_vec_args* targs = (struct _reduce_vec_args*)args;
+    double min = 0.0;
+    for (size_t j = 0; j < targs->matp->cols; ++j) {
+        if (double_matrix_get(targs->matp, targs->row, j, &min))
+            return (void*) ((intptr_t) -1);
+        if (min > 0.0) break;
+    }
+
+    if (min == 0.0)
+        return (void*) ((intptr_t) -1);
+
+    for (size_t j = 0; j < targs->matp->cols; ++j) {
+        double value = 0.0;
+        if (double_matrix_get(targs->matp, targs->row, j, &value))
+            return (void*) ((intptr_t) -1);
+        targs->X += is_even_as_integer(value / min) ? sin(value) : 0.0;
+    }
+    return (void*) ((intptr_t) 0);
+}
+
+int reduce(struct matrix* matp, double* reduction)
+{
+    double X = 0.0;
+    if (!matp || !reduction) return -1;
+
+    struct _reduce_vec_args tasks_args[matp->rows];
+    for (size_t i = 0; i < matp->rows; ++i) {
+        tasks_args[i].matp = matp;
+        tasks_args[i].row = i;
+        tasks_args[i].X = 0.0;
+        if (!ptpool_enqueue_task(pool, _reduce_vec_routine, &tasks_args[i]))
+            return -1;
+    }
+
+    ptpool_wait(pool);
+    for (size_t i = 0; i < matp->rows; ++i)
+        X += tasks_args[i].X;
+    *reduction += X;
+    return 0;
+}
+#elif defined(_OPENMP)
 int reduce(struct matrix* matp, double* reduction)
 {
     int ret = 0;
