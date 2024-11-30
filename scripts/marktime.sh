dc697a3a90efbea60cbb1f11769b402c7b2cdf86
come_ill_foo <rayveleaveme34@inbox.ru>
2024-11-30 23:42:36 +0000

    feat(scripts): parse time from program

---
+++ scripts/marktime.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+usage()
+{
+    cat <<EOF
+Usage: ${0##*/} [options] benchmark N
+
+Options:
+    -h, --help  Prints this help message
+EOF
+    exit 22 # EINVAL: Invalid argument
+}
+
+while true; do
+    case $1 in
+        -h|--help)
+            usage
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+[ $# -lt 2 ] && usage
+
+# @brief path to program under measurements
+benchmark="$1"
+
+# @brief argument for benchmark N
+N="$2"
+set -ueo pipefail
+
+if [ ! -f "${benchmark}" ]; then
+    echo "Unable to find benchmark ${benchmark}"
+    exit 2 # ENOENT: No such file or directory
+fi
+
+if [ "${N}" -lt 2 ]; then
+    echo "Invalid value for N (${N}), should be not less than 2"
+    exit 22 # EINVAL: Invalid argument
+fi
+"${benchmark}" "${N}" | tail -n1 | grep -Eo '[0-9]+$'
