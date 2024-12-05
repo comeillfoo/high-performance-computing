a483437bca457f0320119e73b95bfd6a84adf7ae
come_ill_foo <rayveleaveme34@inbox.ru>
2024-12-05 22:34:58 +0000

    feat: enchance scalability graphs

--- scripts/plot-scalability.sh
+++ scripts/plot-scalability.sh
@@ -64,6 +64,10 @@ esac)
 # @brief plot title
 plot_title=$(echo "Исследование масштабируемости при\nпараллелизации сортировки ${sort_impl}\n(матрицы - ${matrices_impl})")
 
+# @brief output path
+output="${csvtable/.csv/.png}"
+output="${output/data/assets}"
+
 gnuplot -p <<EOF
 set ylabel 'Время, мс'
 set xlabel 'N'
@@ -72,13 +76,25 @@ set style line 100 lt 1 lc rgb "grey" lw 0.5 # linestyle for the grid
 set grid ls 100 # enable grid with specific linestyle
 set datafile separator ','
 set key autotitle columnhead # use the first line as title
-set terminal qt size 1280,720 persist
+set terminal png size 1280,720 enhanced font "Helvetica,14"
+set output '${output}'
 set key right bottom
 
 set xtics 2
 set ytics 10
 
+set style line 2 lw 3 lt rgb "#f62aa0" # (pink)
+set style line 3 lw 3 lt rgb "#26dfd0" # (light blue)
+set style line 4 lw 3 lt rgb "#b8ee30" # (limegreen)
+set style line 5 lw 3 lt rgb "#f2d338"
+set style line 6 lw 3 lt rgb "#f2622e"
+set style line 7 lw 3 lt rgb "#f23030"
+set style line 8 lw 3 lt rgb "#248ea6"
+set style line 9 lw 3 lt rgb "#005148"
+set style line 10 lw 3 lt rgb "#a6bc09"
+
 stats '${csvtable}'
 N=STATS_columns
-plot for [i=2:N] '${csvtable}' using 1:i with lines
+plot for [i=2:N] '${csvtable}' using 1:i with lines ls i
 EOF
+echo "Saved PNG to ${output}"
